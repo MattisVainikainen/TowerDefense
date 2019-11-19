35e401ed2fc9c85f006242dc37a24db8af834683
Mattis Vainikainen <Mattis.vainikainen@gmail.com>
2019-11-19 16:19:20 +0100

    Added Beta waveSpawner

---
+++ TowerDefense/Assets/WaveSpawner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countDown = 2f;
+    private int waveIndex = 0;
+
+    public Text waveCountdownText;
+
+    private void Update()
+    {
+        if(countDown <= 0)
+        {
+            StartCoroutine(SpawnWave());
+            countDown = timeBetweenWaves;
+
+        }
+
+        countDown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Round(countDown).ToString(); 
+    }
+
+    IEnumerator SpawnWave() 
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation); 
+    }
+
+
+}
